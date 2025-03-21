058a35d86873aeaad0f02ad4ae3cc83ef1dae97c
RAZIYAA <114641285+raziya21@users.noreply.github.com>
2025-03-22 00:48:45 +0530

    Update settings.py

--- HOPE/Hope/Hope/settings.py
+++ HOPE/Hope/Hope/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6lga#vwabe7s=n=wxzp^*_!bh2p0q57!7(db1+=6%n6f^*$gu)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hopeapp'
+    'Hope'
 ]
 
 MIDDLEWARE = [
